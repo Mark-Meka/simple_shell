ebbdb675df547d61e2e616dea942cab63f878e97
Mark-Meka <marcomeka2003@gmail.com>
2023-09-13 01:27:55 +0300

    Mark

--- shell.c
+++ shell.c
@@ -2,10 +2,9 @@
 
 int main(int ac, char **argv)
 {
-	char *command = 1024;
+	char *command = NULL, *command_copy = NULL;
 	size_t len = 0;
 	ssize_t reader;
-	const char *delim = " \n";
 	int token_num = 0;
 	char *token;
 	int i;
@@ -14,31 +13,30 @@ int main(int ac, char **argv)
 	(void)ac;
 
 	/* infite loop */
-	while(1)
+	while (1)
 	{
 		printf("$ ");
 		reader = getline(&command, &len, stdin);
-		
 		/* check if getlind faild or not */
 		if (reader == -1)
 		{
 			printf("Exiting Shell......\n");
 			return (-1);
 		}
-		if (command == -1)
+		if (command_copy == NULL)
 		{
 			perror("Allocation Error");
 			return (1);
 		}
-		
+		strcpy(command_copy, command);
 		/************** split the string (command) & returns an array of each word *************/
 		/* calculate the total number of token */
-		token = strtok(command, delim);
+		token = strtok(command, " \n");
 
 		while (token != NULL)
 		{
 			token_num++;
-			token = strtok(NULL, delim);
+			token = strtok(NULL, " \n");
 		}
 		token_num++;
 
@@ -46,12 +44,25 @@ int main(int ac, char **argv)
 		argv = malloc(sizeof(char *) * token_num);
 
 		/* store each token in the array */
-		token = strtok(
+		token = strtok(command_copy, " \n");
+
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
+
+			token = strtok(NULL, " \n");
+		}
+		argv[i] = NULL;
+
+
 
 		printf("%s\n", command);
 
 		/* free up allocated memory */
+		free(argv);
 		free(command);
+		free(command_copy);
 	}
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -4,3 +4,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
