d838a2cc7c382b0bbdc90e7bdf6c548d4c2adc32
Mark-Meka <marcomeka2003@gmail.com>
2023-09-20 19:38:32 +0300

    Mark

--- shell3.c
+++ shell3.c
@@ -1,4 +1,9 @@
 #include "shell.h"
+/**
+ * main - Entry point for the simple_shell.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	char input[MAX_INPUT_LENGTH];
@@ -6,7 +11,7 @@ int main(void)
 	while (1)
 	{
 		printf("simple_shell$ ");
-		
+
 		if (!fgets(input, sizeof(input), stdin))
 			break;
 		input[strcspn(input, "\n")] = '\0';
@@ -14,14 +19,16 @@ int main(void)
 		if (access(input, X_OK) == 0)
 		{
 			pid_t pid = fork();
+
 			if (pid == -1)
 			{
 				perror("fork");
 				exit(EXIT_FAILURE);
 			}
-			if (pid == 0)\
+			if (pid == 0)
 			{
 				char *args[] = {input, NULL};
+
 				execve(input, args, environ);
 				perror("execve");
 				exit(EXIT_FAILURE);
@@ -29,6 +36,7 @@ int main(void)
 			else
 			{
 				int status;
+
 				waitpid(pid, &status, 0);
 			}
 		}
@@ -37,5 +45,5 @@ int main(void)
 			fprintf(stderr, "%s: command not found\n", input);
 		}
 	}
-	return 0;
+	return (0);
 }
