503888513cdc816b1f678d623c01d38921c8acec
Mark-Meka <marcomeka2003@gmail.com>
2023-09-20 18:35:05 +0300

    Mark

--- shell.c
+++ shell.c
@@ -1,74 +1,33 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-void sigintHandler(int signo)
+#define MAX_INPUT_LENGTH 1024
+
+/**
+ * main - Entry point for the simple_shell.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-     printf("\n");
-}
+	char input[MAX_INPUT_LENGTH], *args[2];
 
-int main(int argc, char **argv)
-{
-    char *command = NULL;
-    size_t len = 0;
-    char *stkn;
-    pid_t my_pid;
-    int status;
+	while (printf("simple_shell$ ") && fgets(input, sizeof(input), stdin))
+	{
+		input[strcspn(input, "\n")] = '\0';
 
-    signal(SIGINT, sigintHandler);
-
-    while (1)
-    {
-        printf("$ ");
-        getline(&command, &len, stdin);
-
-        command[strcspn(command, "\n")] = '\0';
-
-        if (strcmp(command, "exit") == 0)
-        {
-            free(command);
-            exit(0);
-        }
-
-        char *arr[MAX_ARGUMENTS];
-        int i = 0;
-        stkn = strtok(command, " ");
-        while (stkn != NULL && i < MAX_ARGUMENTS - 1)
-        {
-            arr[i] = stkn;
-            stkn = strtok(NULL, " ");
-            i++;
-        }
-        arr[i] = NULL;
-
-        my_pid = fork();
-        if (my_pid == -1)
-        {
-            perror("Error");
-            free(command);
-            exit(1);
-        }
-        else if (my_pid == 0)
-        {
-
-            if (execvp(arr[0], arr) == -1)
-            {
-                perror("execvp");
-                free(command);
-                exit(1);
-            }
-        }
-        else
-        {
-
-            if (i > 0 && strcmp(arr[i - 1], "&") == 0)
-            {
-                printf("Running in the background: %s\n", arr[0]);
-            }
-            else
-            {
-                wait(&status);
-            }
-        }
-    }
-    free(command);
-    return 0;
+		if (fork() == 0)
+		{
+			args[0] = input;
+			args[1] = NULL;
+			execve(input, args, NULL);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		wait(NULL);
+	}
+	return (0);
 }
