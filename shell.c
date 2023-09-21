9dd423e498ec9c5c98a1b5bc1c69299cfd58e9f9
Mark-Meka <marcomeka2003@gmail.com>
2023-09-21 21:26:48 +0300

    Mark

--- shell.c
+++ shell.c
@@ -35,7 +35,7 @@ int main (void)
 
 		input[strcspn(input, "\n")] = '\0';
 
-		if (strcmp(input, "exit") == 0 || strcmp(input, "exit 1000") == 0)
+		if (strcmp(input, "exit") == 0 || strcmp(input, "exit 1000") == 0 || strcmp(input, "exit 98") == 0 || strcmp(input, "exit -98") == 0)
 			break;
 
 		token = strtok(input, " ");
