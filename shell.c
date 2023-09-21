98e78d813b9740591d1ea90c382b521a121df8ce
Mark-Meka <marcomeka2003@gmail.com>
2023-09-21 10:11:40 +0300

    Mark

--- shell.c
+++ shell.c
@@ -12,21 +12,28 @@
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH], *args[2];
-	args[2] = NULL;
+	char input[MAX_INPUT_LENGTH], *args[MAX_INPUT_LENGTH / 2 + 1];
+	args[MAX_INPUT_LENGTH / 2] = NULL;
+	
 	while (printf("simple_shell$ ") && fgets(input, sizeof(input), stdin))
 	{
 		input[strcspn(input, "\n")] = '\0';
+
+		if (strcmp(input, "exit") == 0)
+			break;
+		int i =0;
+		char *token = strtok(input, " ");
+		
+		while (token != NULL)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
 		if (fork() == 0)
 		{
-			args[0] = input;
-			args[1] = NULL;
-			
-			if (strcmp(input, "exit") == 0)
-				break;
-
-			execve(input, args, NULL);
-			perror("execve");
+			execvp(args[0], args);
+			perror("execvp");
 			exit(EXIT_FAILURE);
 		}
 		wait(NULL);
