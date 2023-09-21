872eb5b87725a08f3bff50a0ea026dd6f412d7e0
Mark-Meka <marcomeka2003@gmail.com>
2023-09-21 03:59:26 +0300

    Mark

---
+++ shell.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define MAX_INPUT_LENGTH 1024
+/**
+ * main - Entry point for the simple_shell.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char input[MAX_INPUT_LENGTH], *args[2];
+
+	while (printf("simple_shell$ ") && fgets(input, sizeof(input), stdin))
+	{
+		input[strcspn(input, "\n")] = '\0';
+		if (fork() == 0)
+		{
+			args[0] = input;
+			args[1] = NULL;
+			execve(input, args, NULL);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		wait(NULL);
+	}
+    return (0);
+}
