7c0711129ebed62179d9f222183a80663bba9c3a
Mark-Meka <marcomeka2003@gmail.com>
2023-09-21 11:54:12 +0300

    Mark

--- shell.c
+++ shell.c
@@ -7,11 +7,6 @@
 int main(void)
 {
 	char input[MAX_INPUT_LENGTH], *args[2];
-	char *path = getenv("PATH");
-	char full_path[MAX_INPUT_LENGTH];
-	char *command = input;
-
-	printf("Betty rules!\n");
 
 	while (printf("simple_shell$ ") && fgets(input, sizeof(input), stdin))
 	{
@@ -20,32 +15,15 @@ int main(void)
 		if (strcmp(input, "exit") == 0)
 			break;
 
-		while ((command = strsep(&path, ":")))
+		if (fork() == 0)
 		{
-			printf(full_path, "%s/%s", command, input);
-			if (access(full_path, F_OK) == 0)
-				break;
-		}
-
-		if (command == NULL)
-		{
-			printf("Command does not exist: %s\n", input);
-			continue;
-		}
-		pid_t pid = fork();
-
-		if (pid == 0)
-		{
-			args[0] = full_path;
+			args[0] = input;
 			args[1] = NULL;
-			execve(full_path, args, NULL);
-			perror("execve");
+			execve(input, args, NULL);
+			perror("exit");
 			exit(EXIT_FAILURE);
 		}
-		else
-		{
-			wait(NULL);
-		}
+		wait(NULL);
 	}
 	return (0);
 }
