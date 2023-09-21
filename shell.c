5fc0680f08a507fd3148f08efa81f5e001cd4ecb
Mark-Meka <marcomeka2003@gmail.com>
2023-09-21 10:21:46 +0300

    Mark

--- shell.c
+++ shell.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
-#define MAX_INPUT_LENGTH 1024
+#include "shell.h"
 /**
  * main - Entry point for the simple_shell.
  *
@@ -12,26 +6,21 @@
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH], *args[MAX_INPUT_LENGTH / 2 + 1];
-	args[MAX_INPUT_LENGTH / 2] = NULL;
-	
+	char input[MAX_INPUT_LENGTH], *args[3];
+
+	args[2] = NULL;
+
 	while (printf("simple_shell$ ") && fgets(input, sizeof(input), stdin))
 	{
 		input[strcspn(input, "\n")] = '\0';
 
 		if (strcmp(input, "exit") == 0)
 			break;
-		int i =0;
-		char *token = strtok(input, " ");
-		
-		while (token != NULL)
-		{
-			args[i++] = token;
-			token = strtok(NULL, " ");
-		}
-		args[i] = NULL;
+
 		if (fork() == 0)
 		{
+			args[0] = "ls";
+			args[1] = "-laR";
 			execvp(args[0], args);
 			perror("execvp");
 			exit(EXIT_FAILURE);
