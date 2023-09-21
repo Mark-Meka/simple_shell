1352a9f3e9ee3208a54bfd5d77563e53b4f87f5f
Mark-Meka <marcomeka2003@gmail.com>
2023-09-21 12:25:40 +0300

    Mark

--- shell.c
+++ shell.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-	char input[MAX_INPUT_LENGTH], *args[4];
-	char *token;
+	char input[MAX_INPUT_LENGTH], *args[4], *token;
 
 	while (printf("simple_shell$ ") && fgets(input, sizeof(input), stdin))
 	{
@@ -20,9 +19,25 @@ int main(void)
 		args[0] = token;
 		args[1] = NULL;
 
+		if (strcmp(args[0], "/bin/ls") == 0)
+		{
+			token = strtok(NULL, " ");
+			if (token != NULL && strcmp(token, "-l") == 0)
+			{
+				args[1] = "-l";
+				token = strtok(NULL, " ");
+			}
+			else if (token != NULL)
+			{
+				args[1] = token;
+				token = strtok(NULL, " ");
+			}
+		}
+		args[2] = token;
+		args[3] = NULL;
+
 		if (access(args[0], F_OK) == 0)
 		{
-
 			if (fork() == 0)
 			{
 				execve(args[0], args, NULL);
